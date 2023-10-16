983a2621e77feec551c258baca9232ea6a2832be
dikshant-honda <dikshant_s@jp.honda>
2023-10-16 16:24:54 +0900

    second client test done

---
+++ client_2.py
@@ -0,0 +1,34 @@
+import socket
+import cv2
+import pickle
+import struct
+import imutils
+
+camera = False
+if camera == True:
+    vid = cv2.VideoCapture(0)
+else:
+    vid = cv2.VideoCapture('/home/dikshant/Videos/sample.mp4')
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_ip = '192.168.55.1'  # Here according to your server ip write the address
+
+port = 9999
+client_socket.connect((host_ip, port))
+
+if client_socket:
+    while (vid.isOpened()):
+        try:
+            img, frame = vid.read()
+            frame = imutils.resize(frame, width=380)
+            a = pickle.dumps(frame)
+            message = struct.pack("Q", len(a))+a
+            sent = client_socket.sendall(message)
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            cv2.imshow(f"TO: {host_ip}", frame)
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("q"):
+                client_socket.close()
+        except:
+            print('VIDEO FINISHED!')
+            break
